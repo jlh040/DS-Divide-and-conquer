345142f8fcfd698433967fc810dff82bc1899927
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-29 15:39:59 -0700

    implemented find-rotation-count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,26 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (arr[middle - 1] > arr[middle]) {
+      return middle;
+    }
+    else if (arr[middle + 1] < arr[middle]) {
+      return middle + 1;
+    }
+    else if (arr[left] > arr[middle]) {
+      right = middle - 1;
+    }
+    else if (arr[right] < arr[middle]) {
+      left = middle + 1;
+    }
+    else {
+      return 0;
+    }
+  }
 }
 
-module.exports = findRotationCount
+module.exports = findRotationCount;
--- find-rotation-count.test.js
+++ find-rotation-count.test.js
@@ -5,5 +5,8 @@ describe("#findRotationCount", function () {
     expect(findRotationCount([15, 18, 2, 3, 6, 12])).toBe(2)
     expect(findRotationCount([7, 9, 11, 12, 5])).toBe(4)
     expect(findRotationCount([7, 9, 11, 12, 15])).toBe(0)
+    expect(findRotationCount([45, 14, 18, 22, 35, 36, 39])).toBe(1);
+    expect(findRotationCount([4, 6, 10, 13, 22, 67, 89])).toBe(0);
+    expect(findRotationCount([432, 450, 489, 33, 57, 88, 91, 249])).toBe(3);
   })
 })
