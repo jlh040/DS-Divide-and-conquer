86de089628588eb3e8118e0184c0fa2b6b57f87d
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-29 11:54:33 -0700

    implemented find-rotated-index

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,34 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, val) {
+  let left;
+  let right;
+  let middle = Math.floor((arr.length - 1) / 2);
+  if (val === arr[middle]) {
+    return middle;
+  }
+  else if (val <= arr[arr.length - 1]) {
+    left = middle + 1;
+    right = arr.length - 1
+  }
+  else {
+    left = 0;
+    right = middle - 1;
+  }
+
+  while (left <= right) {
+    middle = Math.floor((left + right) / 2);
+
+    if (arr[middle] === val) {
+      return middle;
+    }
+    else if (arr[middle] < val) {
+      left = middle + 1;
+    }
+    else {
+      right = middle - 1;
+    }
+  }
+
+  return -1;
 }
 
 module.exports = findRotatedIndex
