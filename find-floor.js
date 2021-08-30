22b7ae6cc56f4caabebd3b8780e761c1c7fa04b8
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-29 17:24:20 -0700

    implemented find-floor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,31 @@
-function findFloor() {
-  
+function findFloor(arr, val) {
+  let left = 0;
+  let right = arr.length - 1;
+  let floor;
+  if (arr[left] > val) {
+    return -1;
+  }
+  else if (arr[right] <= val) {
+    return arr[right];
+  }
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (arr[middle] === val) {
+      return arr[middle];
+    }
+    else if (arr[middle] < val) {
+      floor = arr[middle];
+      left = middle + 1;
+    }
+    else {
+      right = middle - 1;
+    }
+  };
+
+  return floor;
 }
 
 module.exports = findFloor
+
--- find-floor.test.js
+++ find-floor.test.js
@@ -5,6 +5,8 @@ describe("#findFloor", function(){
     expect(findFloor([1, 2, 8, 10, 10, 12, 19], 5)).toBe(2) // 2
     expect(findFloor([1, 2, 8, 10, 10, 12, 19], 20)).toBe(19) // 19
     expect(findFloor([1, 2, 8, 10, 10, 12, 19], 0)).toBe(-1) // -1
-
+    expect(findFloor([1, 3, 4, 4, 5, 6, 7, 8, 12, 15, 18], 30)).toBe(18);
+    expect(findFloor([1, 3, 4, 4, 5, 6, 7, 8, 12, 15, 18], 17)).toBe(15);
+    expect(findFloor([1, 3, 4, 4, 5, 6, 7, 8, 12, 15, 18], 8)).toBe(8);
   })
 })
